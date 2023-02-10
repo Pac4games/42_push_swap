30aa9d726cddb50f2f4b85ecb2837eabbadb00a3
Pac4games <amanoskull42@gmail.com>
2023-02-10 13:12:05 +0000

    Bro I don't even know what I'm doing anymore

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: paugonca <paugonca@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/31 16:31:06 by paugonca          #+#    #+#             */
-/*   Updated: 2023/02/09 16:14:55 by paugonca         ###   ########.fr       */
+/*   Updated: 2023/02/10 11:58:01 by paugonca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@
 # include "../libft/libft.h"
 # include <limits.h>
 # include <unistd.h>
+# include <stdio.h>
 
 //check_args.c
 int	check_nums(int ac, char **av);
 int	check_limit(int ac, char **av);
 int	check_dups(int ac, char **av);
-int	check_sign(int ac, char **av);
 int	check_args(int ac, char **av);
 
 #endif
